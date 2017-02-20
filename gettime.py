5ef27da773ddd082735f4281a79ed763b05ed6b4
CoderCheese <sabethpathen1360@gmail.com>
2017-02-20 13:45:02 -0500

    Update gettime.py

--- gettime.py
+++ gettime.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 now = datetime.now()
-print "Here is the time:"
+#This will show the time in a 24-hour format
+print "Here is the time(The time is shown in 24-hour format:"
 print '%s ' % (now.minute) + "minutes have passed since " + '%s ' % (now.hour) + "o'Clock"
 print "The date is " + '%s/%s/%s' % (now.month, now.day, now.year)
